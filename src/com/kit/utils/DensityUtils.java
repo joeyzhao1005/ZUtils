fdbf0c733543e9362a3bfc6d7d4b95ef3cdd6e76
laozhao1005 <laozhao1005@gmail.com>
2017-03-19 23:17:56 +0800

    调整 icon 大小

--- src/com/kit/utils/DensityUtils.java
+++ src/com/kit/utils/DensityUtils.java
@@ -8,7 +8,7 @@ public class DensityUtils {
      * 根据手机的分辨率从 dp 的单位 转成为 px(像素)
      */
     public static int dip2px(Context context, float dpValue) {
-        final float scale = context.getResources().getDisplayMetrics().density;
+        final float scale = getScale(context);
         return (int) (dpValue * scale + 0.5f);
     }
 
@@ -16,7 +16,7 @@ public class DensityUtils {
      * 根据手机的分辨率从 px(像素) 的单位 转成为 dp
      */
     public static int px2dip(Context context, float pxValue) {
-        final float scale = context.getResources().getDisplayMetrics().density;
+        final float scale = getScale(context);
         return (int) (pxValue / scale + 0.5f);
     }
 
@@ -25,7 +25,7 @@ public class DensityUtils {
      * 根据手机的分辨率从 dp 的单位 转成为 px(像素)
      */
     public static int dip2px(float dpValue) {
-        final float scale = ResWrapper.getInstance().getResources().getDisplayMetrics().density;
+        final float scale = getScale();
         return (int) (dpValue * scale + 0.5f);
     }
 
@@ -33,9 +33,20 @@ public class DensityUtils {
      * 根据手机的分辨率从 px(像素) 的单位 转成为 dp
      */
     public static int px2dip(float pxValue) {
-        final float scale = ResWrapper.getInstance().getResources().getDisplayMetrics().density;
+        final float scale = getScale();
         return (int) (pxValue / scale + 0.5f);
     }
 
+    public static float getScale(Context context){
+        final float scale = context.getResources().getDisplayMetrics().density;
+
+        return scale;
+    }
+
+    public static float getScale(){
+        final float scale = ResWrapper.getInstance().getResources().getDisplayMetrics().density;
+
+        return scale;
+    }
 
 }
