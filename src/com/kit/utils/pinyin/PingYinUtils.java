affb3bbb35197c33448b00d766fcf6b883f39251
laozhao1005 <laozhao1005@gmail.com>
2017-04-01 22:48:13 +0800

    调整图标大小显示逻辑

--- src/com/kit/utils/pinyin/PingYinUtils.java
+++ src/com/kit/utils/pinyin/PingYinUtils.java
@@ -4,6 +4,7 @@ import net.sourceforge.pinyin4j.PinyinHelper;
 import net.sourceforge.pinyin4j.format.HanyuPinyinCaseType;
 import net.sourceforge.pinyin4j.format.HanyuPinyinOutputFormat;
 import net.sourceforge.pinyin4j.format.HanyuPinyinToneType;
+import net.sourceforge.pinyin4j.format.HanyuPinyinVCharType;
 import net.sourceforge.pinyin4j.format.exception.BadHanyuPinyinOutputFormatCombination;
 
 /**
@@ -36,7 +37,7 @@ public class PingYinUtils {
                 HanyuPinyinOutputFormat();
         format.setCaseType(HanyuPinyinCaseType.LOWERCASE);
         format.setToneType(HanyuPinyinToneType.WITHOUT_TONE);
-//        format.setVCharType(HanyuPinyinVCharType.WITH_V);
+        format.setVCharType(HanyuPinyinVCharType.WITH_V);
 
         char[] input = inputString.trim().toCharArray();
         String output = "";
