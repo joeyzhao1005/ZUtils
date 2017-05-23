fc7f4a845b659d020e76b24508c720a57c3fe185
Joey <laozhao1005@gmail.com>
2017-05-23 13:57:49 +0800

    markdown ok

--- src/com/kit/utils/AssetsUtils.java
+++ src/com/kit/utils/AssetsUtils.java
@@ -5,22 +5,39 @@ import android.content.Context;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-public class AssetsUtils extends  AssetsTxt2String{
-	public static String getFromAssets(Context context, String fileName) {
-		try {
-			InputStreamReader inputReader = new InputStreamReader(context
-					.getResources().getAssets().open(fileName));
-			BufferedReader bufReader = new BufferedReader(inputReader);
-			String line = "";
-			String Result = "";
-			while ((line = bufReader.readLine()) != null)
-				Result += line+"\\n";
-			return Result;
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+public class AssetsUtils extends AssetsTxt2String {
+    public static String getFromAssets(Context context, String fileName) {
+        try {
+            InputStreamReader inputReader = new InputStreamReader(context
+                    .getResources().getAssets().open(fileName));
+            BufferedReader bufReader = new BufferedReader(inputReader);
+            String line = "";
+            String Result = "";
+            while ((line = bufReader.readLine()) != null)
+                Result += line + " \n";
+            return Result;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-		return null;
-	}
+        return null;
+    }
+
+    public static String getFromAssetsNo_N(Context context, String fileName) {
+        try {
+            InputStreamReader inputReader = new InputStreamReader(context
+                    .getResources().getAssets().open(fileName));
+            BufferedReader bufReader = new BufferedReader(inputReader);
+            String line = "";
+            String Result = "";
+            while ((line = bufReader.readLine()) != null)
+                Result += line;
+            return Result;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
 
 }
