2c96249c2f8d2204b07cc65a1605537793987228
laozhao1005 <laozhao1005@gmail.com>
2017-06-02 00:36:06 +0800

    root授权的删掉

--- src/com/kit/utils/system/RootUtils.java
+++ src/com/kit/utils/system/RootUtils.java
@@ -54,7 +54,40 @@ public class RootUtils {
     }
 
 
-
+    /**
+     * 判断当前应用是否有 root 权限
+     *
+     * @return
+     */
+    public static synchronized boolean isRootPermission() {
+        Process process = null;
+        DataOutputStream os = null;
+        try {
+            process = Runtime.getRuntime().exec("su");
+            os = new DataOutputStream(process.getOutputStream());
+            os.writeBytes("exit\n");
+            os.flush();
+            int exitValue = process.waitFor();
+            if (exitValue == 0) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (Exception e) {
+            ZogUtils.d("Unexpected error - Here is what I know: "
+                    + e.getMessage());
+            return false;
+        } finally {
+            try {
+                if (os != null) {
+                    os.close();
+                }
+                process.destroy();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
     /**
      * 模拟按键
