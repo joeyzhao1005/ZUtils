74b5decad2adece1cf1eeeb1a906f80da33ebb65
Zhao <laozhao1005@gmail.com>
2017-09-05 15:45:18 +0800

    调节大小 OK 名人名言 ok

--- src/com/kit/utils/DateUtils.java
+++ src/com/kit/utils/DateUtils.java
@@ -26,8 +26,44 @@ public class DateUtils {
     public static String WEIBO_DATE = "EEE MMM d HH:mm:ss Z yyyy";
 
 
+	/**
+	 * 获取随机日期
+	 *
+	 * @param beginDate
+	 *            起始日期，格式为：yyyy-MM-dd
+	 * @param endDate
+	 *            结束日期，格式为：yyyy-MM-dd
+	 * @return
+	 */
 
-    public static Date getDateFormLong(long dateLong) {
+	public static Date randomDate(String beginDate, String endDate) {
+		try {
+			SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+			Date start = format.parse(beginDate);// 构造开始日期
+			Date end = format.parse(endDate);// 构造结束日期
+			// getTime()表示返回自 1970 年 1 月 1 日 00:00:00 GMT 以来此 Date 对象表示的毫秒数。
+			if (start.getTime() >= end.getTime()) {
+				return null;
+			}
+			long date = random(start.getTime(), end.getTime());
+
+			return new Date(date);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	private static long random(long begin, long end) {
+		long rtn = begin + (long) (Math.random() * (end - begin));
+		// 如果返回的是开始时间和结束时间，则递归调用本函数查找随机值
+		if (rtn == begin || rtn == end) {
+			return random(begin, end);
+		}
+		return rtn;
+	}
+
+	public static Date getDateFormLong(long dateLong) {
 
 		Date sd = new Date(dateLong);
 		return sd;
