dab3b4caad0e5b142588da3d48d2ba839c390b7a
joeyzhao <joeyzhao1005@gmail.com>
2018-05-22 18:54:55 +0800

    设置项调整

---
+++ src/main/java/com/kit/utils/ApiLevel.java
@@ -0,0 +1,23 @@
+package com.kit.utils;
+
+import android.os.Build;
+
+public class ApiLevel {
+    public static final boolean ATLEAST_LOLLIPOP_MR1 =
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP_MR1;
+
+    public static final boolean ATLEAST_LOLLIPOP =
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP;
+
+    public static final boolean ATLEAST_KITKAT =
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
+
+    public static final boolean ATLEAST_JB_MR1 =
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1;
+
+    public static final boolean ATLEAST_JB_MR2 =
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2;
+
+    public static final boolean ATLEAST_JELLY_BEAN =
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN;
+}
