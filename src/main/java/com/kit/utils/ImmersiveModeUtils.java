f8cca0f5302d966f2f2d86739040fcace07055bd
laozhao1005 <laozhao1005@gmail.com>
2018-07-29 19:11:00 +0800

    完善边栏

--- src/main/java/com/kit/utils/ImmersiveModeUtils.java
+++ src/main/java/com/kit/utils/ImmersiveModeUtils.java
@@ -34,33 +34,7 @@ public class ImmersiveModeUtils {
      * @param navigationBarColor 导航栏的颜色
      */
     public static void immersiveAboveAPI19(AppCompatActivity baseActivity, int statusBarColor, int navigationBarColor, boolean isDarkStatusBarIcon) {
-//        try {
-//            Window window = baseActivity.getWindow();
-//
-//            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT && Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
-//                //4.4版本及以上 5.0版本及以下
-//
-//                window.setFlags(
-//                        WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS,
-//                        WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-//            } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-//                //5.0版本及以上
-//                window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
-//                        | WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
-//                if (isDarkStatusBarIcon && Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-//                    window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-//                            | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
-//                } else {
-//                    window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
-//                }
-//                window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-//                window.setStatusBarColor(statusBarColor);
-//                window.setNavigationBarColor(navigationBarColor);
-//            }
-//        } catch (Exception e) {
-//        }
-
-        immersiveAboveAPI19(baseActivity, statusBarColor, navigationBarColor, isDarkStatusBarIcon);
+        immersiveAboveAPI19(baseActivity, false, false, statusBarColor, navigationBarColor, isDarkStatusBarIcon);
     }
 
 
