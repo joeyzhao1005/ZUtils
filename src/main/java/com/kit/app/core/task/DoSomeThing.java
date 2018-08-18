65fbbfb486d4ef6d9ad57ec28029f669126b3375
joeyzhao <joeyzhao1005@gmail.com>
2018-08-18 11:27:14 +0800

    icon 匹配

--- src/main/java/com/kit/app/core/task/DoSomeThing.java
+++ src/main/java/com/kit/app/core/task/DoSomeThing.java
@@ -4,5 +4,5 @@ package com.kit.app.core.task;
  * Created by Zhao on 14/11/26.
  */
 public interface DoSomeThing {
-    public void execute(Object... object);
+    void execute(Object... object);
 }
