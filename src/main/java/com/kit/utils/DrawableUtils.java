a6a5c3ff6637218bdb7dba7222d73b40fa30abd0
joeyzhao <joeyzhao1005@gmail.com>
2019-11-25 10:48:28 +0800

    [feature] shortcuts补全

--- src/main/java/com/kit/utils/DrawableUtils.java
+++ src/main/java/com/kit/utils/DrawableUtils.java
@@ -14,6 +14,7 @@ import android.graphics.drawable.Drawable;
 import androidx.annotation.DrawableRes;
 import androidx.core.graphics.drawable.DrawableCompat;
 
+import com.kit.app.application.AppMaster;
 import com.kit.utils.bitmap.BitmapUtils;
 import com.kit.utils.log.Zog;
 
@@ -56,10 +57,8 @@ public class DrawableUtils {
                 height,
                 Bitmap.Config.ARGB_8888);
 
-        Canvas canvas = new Canvas(bitmap);
-
-        //canvas.setBitmap(bitmap);
-
+        Canvas canvas = new Canvas();
+        canvas.setBitmap(bitmap);
 
         drawable.setBounds(0, 0, width, height);
         drawable.draw(canvas);
