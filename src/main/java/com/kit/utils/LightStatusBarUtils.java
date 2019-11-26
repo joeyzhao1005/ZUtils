a8dda535efa81f5230bd89463e6132483f9523e6
joeyzhao1005 <joeyzhao1005@gmail.com>
2019-11-27 02:11:36 +0800

    [fix] 填了个大坑

--- src/main/java/com/kit/utils/LightStatusBarUtils.java
+++ src/main/java/com/kit/utils/LightStatusBarUtils.java
@@ -118,13 +118,13 @@ public class LightStatusBarUtils {
                             window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                                     | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
 //                                    |View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-//                                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+//                                    /*| View.SYSTEM_UI_FLAG_LAYOUT_STABLE*/
                                     | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
                         } else {
                             window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                                     | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
 //                                    |View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-//                                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+//                                    /*| View.SYSTEM_UI_FLAG_LAYOUT_STABLE*/
                             );
                         }
 
@@ -132,11 +132,11 @@ public class LightStatusBarUtils {
                     } else if (!isMarginStatusBar && isMarginNavigationBar) {
                         if (isDarkStatusBarIcon && Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                             window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                                    /*| View.SYSTEM_UI_FLAG_LAYOUT_STABLE*/
                                     | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
                         } else {
                             window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+                                    /*| View.SYSTEM_UI_FLAG_LAYOUT_STABLE*/);
                         }
 
 
@@ -169,11 +169,11 @@ public class LightStatusBarUtils {
             } else if (!isMarginStatusBar && !isMarginNavigationBar) {
                 activity.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
 //                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+                        /*| View.SYSTEM_UI_FLAG_LAYOUT_STABLE*/);
             } else if (!isMarginStatusBar && isMarginNavigationBar) {
 
                 activity.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+                        /*| View.SYSTEM_UI_FLAG_LAYOUT_STABLE*/);
             } else {
                 //留出来状态栏 不留出来导航栏 没找到办法。。
             }
