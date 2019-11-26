f3cf9a5342a4a9c93ba4ddfe94e90c4d96320e4b
joeyzhao1005 <joeyzhao1005@gmail.com>
2019-11-27 02:11:52 +0800

    [fix] 填了个大坑

--- src/main/java/com/kit/utils/LightStatusBarUtils.java
+++ src/main/java/com/kit/utils/LightStatusBarUtils.java
@@ -39,6 +39,7 @@ public class LightStatusBarUtils {
                 break;
 
             case RomUtils.AvailableRomType.NA:
+            default:
                 // N/A do nothing
                 break;
         }
@@ -122,7 +123,7 @@ public class LightStatusBarUtils {
                                     | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
                         } else {
                             window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
 //                                    |View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
 //                                    /*| View.SYSTEM_UI_FLAG_LAYOUT_STABLE*/
                             );
