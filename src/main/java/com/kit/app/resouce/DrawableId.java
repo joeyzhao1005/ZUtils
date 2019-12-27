40b34a16802932c1f8f3b66f924a47b86fcdb38b
joeyzhao <joeyzhao1005@gmail.com>
2019-12-28 00:19:14 +0800

    [fix]  remove rxjava

--- src/main/java/com/kit/app/resouce/DrawableId.java
+++ src/main/java/com/kit/app/resouce/DrawableId.java
@@ -1,7 +1,9 @@
 package com.kit.app.resouce;
 
 /**
- * Created by Zhao on 15/11/20.
+ *
+ * @author Zhao
+ * @date 15/11/20
  */
 public class DrawableId {
     public static final String ic_back = "ic_back";
