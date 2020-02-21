c251d222be0b0ee72a58b3eb513d2ff541c69253
joeyzhao <joeyzhao1005@gmail.com>
2020-02-21 13:28:12 +0800

    [feature] bug修复

--- src/main/java/com/kit/utils/NotificationListenerUtils.java
+++ src/main/java/com/kit/utils/NotificationListenerUtils.java
@@ -17,43 +17,6 @@ import java.util.Set;
 public class NotificationListenerUtils {
 
 
-    /**
-     * 进入设置让用户开启通知监听服务相关权限
-     *
-     * @param context
-     */
-    public void enableNotificationListenerListener(Context context) {
-
-        try {
-//            if (!isNotificationListenerListenerEnabled(context)) {
-            Intent intent = new Intent("android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS");
-            context.startActivity(intent);
-//            } else {
-//                Zog.i("已开启通知监听服务权限");
-//            }
-        } catch (Exception e) {
-            Zog.showException(e);
-
-        }
-    }
-
-
-    /**
-     * 当前应用是否启用了通知监听服务
-     *
-     * @param context
-     * @return
-     */
-    public boolean isNotificationListenerListenerEnabled(Context context) {
-        if (context == null) {
-            context = AppMaster.getInstance().getAppContext();
-            Zog.e("conext is null, it maybe not you wish!");
-        }
-        String pkgName = context.getPackageName();
-        Set<String> packageNames = NotificationManagerCompat.getEnabledListenerPackages(context);
-
-        return packageNames.contains(pkgName);
-    }
 
 
     public static NotificationListenerUtils getInstance() {
